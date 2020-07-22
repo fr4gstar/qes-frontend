59438ddbc3e991bc885cfcfe95ce552f4a72e3a8
sergejbardin <sergej_bardin@yahoo.de>
2020-07-22 10:54:41 +0200

    Heroku configs

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static("./dist/qes-frontend.json"));
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/<name-on-package.json>/" });
+});
+
+app.listen(process.env.PORT || 8080);
